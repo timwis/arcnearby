3842b6ab06a6e441a5466362e3e4f35f61dc4cd5
Tim Wisniewski <tim@timwis.com>
2013-06-08 13:57:34 -0400

    Initial commit

---
+++ arcnearby.js
@@ -0,0 +1,112 @@
+var module = module || {}
+    ,_ = require("underscore")
+    ,request = require("request");
+
+(function(app, _, request) {
+
+    app.options = {
+        apiHost: "http://gis.phila.gov"
+        ,apiPath: "/arcgis/rest/services"
+        ,services: {
+            farmersMarkets: "/PhilaGov/Farmers_Markets/MapServer/0/query"
+            ,cornerStores: "/PhilaGov/Healthy_Corner_Stores/MapServer/0/query"
+        }
+        ,params: {
+            where: "1=1"
+            ,outFields: "*"
+            ,geometry: ""
+            ,geometryType: "esriGeometryEnvelope"
+            ,spatialRel: "esriSpatialRelContains"
+            ,inSR: "4326"
+            ,outSR: "4326"
+            ,returnGeometryOnly: "True"
+            ,returnIdsOnly: "False"
+            ,f: "pjson"
+        }
+    };
+    
+    /**
+     * Gets distance in miles between two latitude/longitude coordinates
+     * No idea how this sorcery works
+     * @param {Array} start Lat/lng coordinate 1
+     * @param {Array} end Lat/lng coordinate 2
+     * @returns {Number} Distance in miles
+     */
+    app.getCoordDistance = function(start, end) {
+        var deg2rad = function(deg) { return deg * (Math.PI / 180); }
+            ,radius = 3963 // radius of the earth in miles
+            ,d = [deg2rad(end[0] - start[0]), deg2rad(end[1] - start[1])]
+            ,a = Math.sin(d[0] / 2) * Math.sin(d[0] / 2) + // No idea wtf is going on these 3 lines
+                    Math.cos(deg2rad(start[0])) * Math.cos(deg2rad(end[0])) *
+                    Math.sin(d[1] / 2) * Math.sin(d[1] / 2)
+            ,c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+            ,distance = radius * c;
+            return distance;
+    };
+    
+    /**
+     * Generates bounding box (square) around a center point with a given radius
+     * @param {Array} coords Latitude/Longitude of the center point
+     * @param {Number} radius Radius of the box in miles
+     * @returns {Array} Bounding box in xmin,ymin,xmax,ymax order ready for an ArcGIS REST query
+     */
+    app.getBoundingBox = function(coords, radius) {
+        var mile = [0.0192, 0.01499] // 1 square mile in degrees (lat, lng)
+            ,boundingBox = [
+                coords[0] - (mile[0] * radius) // xmin/left
+                ,coords[1] - (mile[1] * radius) // ymin/bottom
+                ,coords[0] + (mile[0] * radius) // xmax/right
+                ,coords[1] + (mile[1] * radius) // ymax/top
+            ];
+        console.log(boundingBox);
+        return boundingBox;
+    };
+    
+    /**
+     * Calculates distance from a center point for an array of ArcGIS results, then returns an array of the results sorted by distance
+     * @param {Array} features The "features" object from an ArcGIS REST API result
+     * @param {Array} coords Lat/lng center point to calculate distance from
+     * @returns {Array} Returns features array sorted by new attribute, distance
+     */
+    app.sortByDistance = function(features, coords) {
+        var results = [];
+        _.each(features, function(feature) {
+            feature.attributes.distance = app.getCoordDistance(coords, [feature.geometry.x, feature.geometry.y]);
+            results.push(feature);
+        });
+        results.sort(function(a, b) {
+            return a.attributes.distance - b.attributes.distance;
+        });
+        return results;
+    };
+    
+    /**
+     * Basic serialize helper function, converts object to querystring
+     * @param {Object} obj Object of params
+     * @returns {String} Querystring of params
+     */
+    app.serialize = function(obj) {
+        var str = [];
+        for(var p in obj)
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        return str.join("&");
+    };
+    
+    /**
+     * Executes HTTP request to ArcGIS REST API
+     * @param {String} service Which ArcGIS service to query
+     * @param {Array} boundingBox The xmin,ymin,xmax,ymax bounding box to look for features within
+     * @param {Function} successCallback
+     * @param {Function} errorCallback
+     */
+    app.query = function(service, boundingBox, successCallback, errorCallback) {
+        var params = _.clone(app.options.params);
+        params.geometry = boundingBox;
+        var url = app.options.apiHost + app.options.apiPath + (app.options.services[service] || "") + "?" + app.serialize(params);
+        console.log(url);
+        request(url, function(error, response, body) {
+            (error) ? errorCallback(response, body) : successCallback(response, body);
+        });
+    };
+
+})(module.exports, _, request);
---
+++ main.js
@@ -0,0 +1,18 @@
+var arcnearby = require("./arcnearby")
+    ,_ = require("underscore");
+(function(arcnearby) {
+    
+    var coords = [-75.1661,39.9365]
+        ,boundingBox = arcnearby.getBoundingBox(coords, 1.0);
+    arcnearby.query("cornerStores", boundingBox, function(response, body) {
+        var data = JSON.parse(body);
+        console.log("Found " + data.features.length + " results");
+        var sorted = arcnearby.sortByDistance(data.features, coords);
+        _.each(sorted, function(result) {
+            console.log(result.attributes);
+        });
+    }, function(resopnse) {
+        console.log("Error");
+    });
+    
+})(arcnearby);
