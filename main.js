1084ccd4315c4764ce24466d1bd09e4a20d8246a
Tim Wisniewski <tim@timwis.com>
2013-06-08 16:31:30 -0400

    Added comments to main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
+/**
+ * Example usage of arcnearby
+ */
 var arcnearby = require("./arcnearby")
     ,_ = require("underscore");
-(function(arcnearby) {
-    
-    var coords = [39.9365,-75.1661];
-    arcnearby.getNearby("cornerStores", coords, 0.25, function(results) {
-        _.each(results, function(result) {
-            console.log(result.attributes);
-        });
-    }, function(response, body) {
-        console.log("Error");
+
+var coords = [39.9365,-75.1661];
+arcnearby.getNearby("cornerStores", coords, 0.25, function(results) {
+    _.each(results, function(result) {
+        console.log(result.attributes);
     });
-    
-})(arcnearby);
+}, function(response, body) {
+    console.log("Error");
+});
